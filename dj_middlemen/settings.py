67bd7d1c17c82c3381593fb9648c5eee3c732bf8
Caeden Webb <caedenwebb@unomaha.edu>
2026-03-21 21:58:21 -0500

    Allowed all hosts in settings file for development

--- dj_middlemen/settings.py
+++ dj_middlemen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3c1ee!7tv!d6h!ubht)v9^rtshycx5jrh$i!0s%q9m1qv3scom
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
